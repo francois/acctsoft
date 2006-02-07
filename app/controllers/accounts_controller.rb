13e72c7c028e6b7b2a88f22f5f53534c95fbc56c
fbos <fbos@18ef92ac-1ce2-7148-ac38-1817e6a3da51>
2006-02-07 03:05:26 +0000

    Switched from Ajaxified accounts edit to form edit.
    git-svn-id: svn://svn.teksol.info/repos/projects/acctsoft/trunk@6077 18ef92ac-1ce2-7148-ac38-1817e6a3da51

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,8 +8,25 @@ class AccountsController < ApplicationController
   end
 
   def create
-    self.index
     @account = Account.new
-    @success = @account.update_attributes(params[:account])
+    if @account.update_attributes(params[:account]) then
+      redirect_to :action => :index
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @account = Account.find_by_no(params[:account])
+    raise ActiveRecord::RecordNotFound unless @account
+  end
+
+  def update
+    @account = Account.find(params[:id])
+    if @account.update_attributes(params[:account]) then
+      redirect_to :action => :index
+    else
+      render 'edit'
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def label(object, field, label=nil)
+    %Q(<label for="#{object}_#{field}">#{label ? label : field.to_s.humanize}</label>)
+  end
+
+  def text_field(*args)
+    if args.last.kind_of?(Hash) then
+      args.last[:autocomplete] = 'off' unless args.last.has_key?(:autocomplete)
+    else
+      args << {:autocomplete => 'off'}
+    end
+
+    super(*args)
+  end
 end
