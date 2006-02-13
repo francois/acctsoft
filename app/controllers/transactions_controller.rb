5dbe10e2c7f35c73f752078df4c4f8bd3c03106b
fbos <fbos@18ef92ac-1ce2-7148-ac38-1817e6a3da51>
2006-02-13 03:34:22 +0000

    Allow more formats for date parsing: 20050101, 2005/01/01, 2005/1/1.
    git-svn-id: svn://svn.teksol.info/repos/projects/acctsoft/trunk@6101 18ef92ac-1ce2-7148-ac38-1817e6a3da51

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -64,7 +64,7 @@ class TransactionsController < ApplicationController
   end
 
   def parse_date(date)
-    return date unless date =~ /^(\d{4})-(\d{2})-(\d{2})$/
+    return date unless date =~ /^(?:(\d{4})(\d{2})(\d{2}))(\d{4}).(\d{1,2}).(\d{1,2})$/
     Date.new($1.to_i, $2.to_i, $3.to_i)
   end
 end
