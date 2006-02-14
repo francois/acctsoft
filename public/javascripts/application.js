a03fc3cb3a1add556a1e7d8ece41d9689fed6558
fbos <fbos@18ef92ac-1ce2-7148-ac38-1817e6a3da51>
2006-02-14 01:05:54 +0000

    Automatically calculate the transaction's dt and ct volume on entry.
    git-svn-id: svn://svn.teksol.info/repos/projects/acctsoft/trunk@6126 18ef92ac-1ce2-7148-ac38-1817e6a3da51

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,18 @@
-function updateTransactionVolume(root, selectorExpression, target) {
+function updateTransactionVolume(root, className, target) {
   var volume = 0.0;
 
-  var fields = new Selector(selectorExpression).findElements(root);
-  fields.each(function(field) {
-    alert(field.value.inspect());
-    volume += field.value;
-  });
+  var fields = document.getElementsByClassName(className, root);
+  for (var i = 0; i < fields.length; i++) {
+    match = fields[i].value.match(/(-?\d*\.?\d+?)$/)
+    if (null == match) continue;
 
-  $(target).innerHTML = volume.toString();
+    amount = match[1];
+    volume += parseFloat(amount);
+  }
+
+  var value = (Math.round(volume * 100.0) / 100.0).toString();
+  if (-1 == value.indexOf('.')) value += '.';
+  value += '00';
+  value = value.substr(0, value.indexOf('.') + 3);
+  $(target).innerHTML = '<span class="money"><span class="symbol">$</span>' + value + '</span>';
 }
