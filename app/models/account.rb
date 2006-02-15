0df547eb6fea52b27ec65748669db26392780f3c
fbos <fbos@18ef92ac-1ce2-7148-ac38-1817e6a3da51>
2006-02-15 02:33:33 +0000

    Ensure forced values in Account stay current at all times
    git-svn-id: svn://svn.teksol.info/repos/projects/acctsoft/trunk@6141 18ef92ac-1ce2-7148-ac38-1817e6a3da51

--- app/models/account.rb
+++ app/models/account.rb
@@ -19,10 +19,12 @@ class Account < ActiveRecord::Base
 
   def total_dt_volume=(dt_amount)
     @total_dt_volume = dt_amount
+    @force_dt_total = true
   end
 
   def total_ct_volume=(ct_amount)
     @total_ct_volume = ct_amount
+    @force_ct_total = true
   end
 
   def normally_debitor?
@@ -39,14 +41,15 @@ class Account < ActiveRecord::Base
 
   protected
   def load_total_from_cache(cutoff_date)
+    return if @force_dt_total and @force_ct_total
     return if cutoff_date == @total_volume_cutoff_date
     results = self.class.connection.select_one("
       SELECT SUM(amount_dt_cents) / 100 AS total_dt_volume, SUM(amount_ct_cents) / 100 AS total_ct_volume
       FROM txn_accounts INNER JOIN txns ON txns.id = txn_accounts.txn_id
       WHERE txns.posted_on <= '#{cutoff_date.to_time.strftime('%Y-%m-%d')}' AND txn_accounts.account_id = #{self.id} ")
 
-    @total_dt_volume = results['total_dt_volume'].to_money
-    @total_ct_volume = results['total_ct_volume'].to_money
+    @total_dt_volume = results['total_dt_volume'].to_money unless @force_dt_total
+    @total_ct_volume = results['total_ct_volume'].to_money unless @force_ct_total
     @total_volume_cutoff_date = cutoff_date
   end
 end
