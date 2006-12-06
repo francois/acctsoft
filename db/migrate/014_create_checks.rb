1b3da573c8a17c5e2e01a728170aa70eb60a2bcd
fbos <fbos@18ef92ac-1ce2-7148-ac38-1817e6a3da51>
2006-12-06 16:29:51 +0000

    Made the checks reason be a text, not a string.
    git-svn-id: svn://svn.teksol.info/repos/projects/acctsoft/trunk@8197 18ef92ac-1ce2-7148-ac38-1817e6a3da51

--- db/migrate/014_create_checks.rb
+++ db/migrate/014_create_checks.rb
@@ -2,11 +2,11 @@ class CreateChecks < ActiveRecord::Migration
   def self.up
     create_table :checks do |t|
       t.column :no, :string, :limit => 12
-      t.column :date, :date
+      t.column :written_on, :date
       t.column :beneficiary, :string
       t.column :amount_cents, :integer
       t.column :amount_currency, :string, :limit => 8
-      t.column :reason, :string
+      t.column :reason, :text
       t.column :txn_id, :integer
       t.column :bank_account_id, :integer
     end
