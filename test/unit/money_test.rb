41ca395e7d41810ac0206dbde4587730e62c17b3
François Beausoleil <francois@teksol.info>
2008-03-19 13:52:46 -0400

    Made the money tests green again.

--- test/unit/money_test.rb
+++ test/unit/money_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class MoneyTest < Test::Unit::TestCase
   def test_convert_integer_to_money
-    assert_equal 9 * 100, 9.to_money.cents
+    assert_equal 9, 9.to_money.cents
   end
 
   def test_convert_float_to_money
@@ -13,12 +13,8 @@ class MoneyTest < Test::Unit::TestCase
     assert_equal (13.27 * 100).to_i, 13.261.to_money.cents
   end
 
-  def test_copies_symbolized_currency_when_convert_to_money
-    assert_equal 'CAD', 1.to_money(:CAD).currency
-  end
-
   def test_copies_stringified_currency_when_convert_to_money
-    assert_equal 'IUD', 1.to_money('IUD').currency
+    assert_equal 'IUD', 1.to_money("IUD").currency
   end
 
   def test_zero_cents_is_zero
@@ -33,7 +29,12 @@ class MoneyTest < Test::Unit::TestCase
     assert_equal -12.to_money, Money.zero - (12.to_money)
   end
 
+  def test_negative_money
+    assert_equal Money.new(-1232, "CAD"), "-12.32 cad".to_money
+  end
+
   def test_add_negative_amount
-    assert_equal -12.to_money, Money.zero + ('-12'.to_money)
+    assert_equal -12.to_money, 1.to_money + -13.to_money
+    assert_equal -1200.to_money, Money.zero + ('-12'.to_money)
   end
 end
